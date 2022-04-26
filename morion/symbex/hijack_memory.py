4f1b4a36de113e5be849eb7cd9935bfd7300728e
Damian Pfammatter <pfammatter.damian@gmail.com>
2022-04-26 11:20:24 +0200

    Analyze memory hijacks

---
+++ morion/symbex/hijack_memory.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+## -*- coding: utf-8 -*-
+import argparse
+from   morion.log                           import Logger
+from   morion.symbex.execute                import Executor
+from   morion.symbex.analysis.vulnerability import identify_controllable_memory_reads
+from   morion.symbex.analysis.vulnerability import identify_controllable_memory_writes
+from   triton                               import MODE
+
+
+class MemoryHijacker(Executor):
+
+    def run(self, stepping: bool = False) -> None:
+        # Set symbolic execution mode
+        self._only_on_symbolized = self.ctx.isModeEnabled(MODE.ONLY_ON_SYMBOLIZED)
+        self.ctx.setMode(MODE.ONLY_ON_SYMBOLIZED, True)
+        # Add post-processing function
+        self._post_processing_functions.append(identify_controllable_memory_reads)
+        self._post_processing_functions.append(identify_controllable_memory_writes)
+        # Run symbolic execution
+        super().run(stepping)
+        # Remove post-processing function
+        self._post_processing_functions.pop()
+        self._post_processing_functions.pop()
+        # Restore symbolic execution mode
+        self.ctx.setMode(MODE.ONLY_ON_SYMBOLIZED, self._only_on_symbolized)
+        return
+    
+
+def main() -> None:
+    # Argument parsing
+    description = """
+    Identify potential memory hijacks in a binary's program trace. A memory
+    hijack corresponds to the target of a memory read or write operation being
+    (partly) symbolic.
+    """
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("trace_file",
+                        help="file containing the trace to be executed symbolically")
+    parser.add_argument("--log_level",
+                        choices=["critical", "error", "warning", "info", "debug"],
+                        default="debug",
+                        help="log level")
+    parser.add_argument("--stepping",
+                        action="store_true",
+                        help="Open a debug shell after each instruction")
+    args = parser.parse_args()
+
+    # Symbolic Execution
+    se = MemoryHijacker(Logger(args.log_level))
+    se.load(args.trace_file)
+    se.run(args.stepping)
+    se.store(args.trace_file)
+    return
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
             'morion=morion.symbex.execute:main',
             'morion_backward_slicer=morion.symbex.backward_slice:main',
             'morion_control_hijacker=morion.symbex.hijack_control:main',
-##            'morion_memory_hijacker=',
+            'morion_memory_hijacker=morion.symbex.hijack_memory:main',
 ##            'morion_branch_analyzer=',
 ##            'morion_path_analyzer='
         ]
