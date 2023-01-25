56a1e5fa53141a8efd1d32ad505d1f4821c3217a
Damian Pfammatter <pfammatter.damian@gmail.com>
2023-01-25 15:54:16 +0100

    Trace malloc

--- morion/tracing/gdb/hooking/libc.py
+++ morion/tracing/gdb/hooking/libc.py
@@ -6,6 +6,42 @@ from morion.tracing.gdb.trace       import GdbHelper
 from typing                         import List, Tuple
 
 
+class malloc(base_hook):
+
+    def __init__(self, name: str, entry_addr: int, leave_addr: int, target_addr: int, mode: str = "skip", logger: Logger = Logger()) -> None:
+        super().__init__(name, entry_addr, leave_addr, target_addr, mode, logger)
+        self.synopsis = "void *malloc(size_t size);"
+        return
+
+    def on_entry(self) -> List[Tuple[int, bytes, str, str]]:
+        try:
+            arch = GdbHelper.get_architecture()
+            if arch in ["armv6", "armv7"]:
+                # Log arguments
+                self.size = GdbHelper.get_register_value("r0")
+                self._logger.debug(f"\tsize = {self.size:d}")
+                return super().on_entry()
+            raise Exception(f"Architecture '{arch:s}' not supported.")
+        except Exception as e:
+            self._logger.error(f"{self._name:s} (on=entry, mode={self._mode:s}) failed: {str(e):s}")
+        return []
+
+    def on_leave(self) -> List[Tuple[int, bytes, str, str]]:
+        try:
+            arch = GdbHelper.get_architecture()
+            if arch in ["armv6", "armv7"]:
+                # Log arguments
+                result = GdbHelper.get_register_value("r0")
+                self._logger.debug(f"\tresult = 0x{result:08x}")
+                # Move result to return register r0
+                code = self._arm_mov_to_reg("r0", result)
+                return super().on_leave(code)
+            raise Exception(f"Architecture '{arch:s}' not supported.")
+        except Exception as e:
+            self._logger.error(f"{self._name:s} (on=leave, mode={self._mode:s}) failed: {str(e):s}")
+        return []
+
+
 class memcpy(base_hook):
 
     def __init__(self, name: str, entry_addr: int, leave_addr: int, target_addr: int, mode: str = "skip", logger: Logger = Logger()) -> None:
