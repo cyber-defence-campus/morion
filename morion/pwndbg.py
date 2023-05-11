b952e1ff622a4269853ad928e02cfee0466c27c7
Damian Pfammatter <pfammatter.damian@gmail.com>
2023-05-11 13:14:41 +0200

    Tmux layout to use pwndbg and morion

--- morion/pwndbg.py
+++ morion/pwndbg.py
@@ -1,65 +1,150 @@
 #!/usr/bin/env python3
 ## -*- coding: utf-8 -*-
 import argparse
-import libtmux
+import os
+import subprocess
+from   typing import List
 
-# References
-# - https://libtmux.git-pull.com/reference/windows.html
-# - https://github.com/tmux-python/libtmux
 
+def run_cmd(cmd: List[str]) -> subprocess.CompletedProcess:
+    t = subprocess.run(cmd, capture_output=True, text=True)
+    return t
 
 def main() -> None:
     # Argument parsing
     description = """
-    TODO
+    Use morion together with the GDB-plugin pwndbg.
     """
     parser = argparse.ArgumentParser(description=description,
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument("--gdb",
-    #                     choices=["gdb", "gdb-multiarch"],
-    #                     default="gdb-multiarch",
-    #                     help="Multi-architecture or regular GNU debugger")
+    parser.add_argument('-m', '--multiarch',
+                        action='store_true',
+                        help='use the multi-architecture version of GDB')
+    parser.add_argument('-s', '--tmux_session_name',
+                        default='morion',
+                        help='tmux session name')
+    parser.add_argument('-x', '--gdb_cmd_files',
+                        nargs='*',
+                        help='files with GDB commands to execute')
     args = vars(parser.parse_args())
 
-    session_name = "morion"
+    # Parameters
+    session_name = args['tmux_session_name']
     window_name  = "morion-pwndbg"
+    gdb = "gdb-multiarch" if "multiarch" in args else "gdb"
 
-    # Create tmux session
-    server = libtmux.Server()
-    session = server.new_session(
-        session_name=session_name,
-        kill_session=True,
-        attach=False,
-        window_name=window_name
-    )
+    # Terminal size
+    terminal_size = os.get_terminal_size()
 
-    # Create window and panes
-    window = session.windows.get(window_name=window_name)
-    pane_gdb = window.panes[0]
-    pane_gdb.send_keys("gdb-multiarch -q")
-    pane_morion = window.split_window(
-        attach=False,
-        vertical=False,
-        percent=50
-    )
-    pane_morion.send_keys("morion -h")
-    pane_disasm = pane_gdb.split_window(
-        attach=False,
-        vertical=True,
-        percent=30
-    )
-    pane_gdb.send_keys(
-f"""
+    # Kill a potential previous tmux session
+    p = run_cmd([
+        'tmux', 'kill-session',             # Kill tmux session
+        '-t', session_name                  # Session name
+    ])
+
+    # Create new tmux session and pwndbg pane
+    p = run_cmd([
+        'tmux', 'new-session',                  # New tmux session
+        '-d',                                   # Detach session from current terminal
+        '-P', '-F', '#{pane_id}:#{pane_tty}',   # Print information
+        '-s', session_name,                     # Session name
+        '-x', str(terminal_size.columns),       # Window width
+        '-y', str(terminal_size.lines),         # Window height
+        gdb, '-q'                               # Shell command
+    ])
+    pane_pwndbg = p.stdout.strip().split(':')
+
+    # Create stack pane
+    p = run_cmd([
+        'tmux', 'split-window',                 # Split pane
+        '-d',                                   # Do not change active pane
+        '-v',                                   # Vertical split
+        '-P', '-F', '#{pane_id}:#{pane_tty}',   # Print information
+        '-p', '40',                             # Percentage of new pane
+        '-t', pane_pwndbg[0]                    # Target pane to split
+    ])
+    pane_stack = p.stdout.strip().split(':')
+
+    # Create disassembly pane
+    p = run_cmd([
+        'tmux', 'split-window',                 # Split pane
+        '-d',                                   # Do not change active pane
+        '-h',                                   # Horizontal split
+        '-P', '-F', '#{pane_id}:#{pane_tty}',   # Print information
+        '-p', '70',                             # Percentage of new pane
+        '-t', pane_stack[0]                     # Target pane to split
+    ])
+    pane_disasm = p.stdout.strip().split(':')
+
+    # Create registers pane
+    p = run_cmd([
+        'tmux', 'split-window',                 # Split pane
+        '-d',                                   # Do not change active pane
+        '-h',                                   # Horizontal split
+        '-P', '-F', '#{pane_id}:#{pane_tty}',   # Print information
+        '-p', '50',                             # Percentage of new pane
+        '-t', pane_disasm[0]                    # Target pane to split
+    ])
+    pane_regs = p.stdout.strip().split(':')
+
+    # Create backtrace pane
+    p = run_cmd([
+        'tmux', 'split-window',                 # Split pane
+        '-d',                                   # Do not change active pane
+        '-v',                                   # Vertical split
+        '-P', '-F', '#{pane_id}:#{pane_tty}',   # Print information
+        '-p', '50',                             # Percentage of new pane
+        '-t', pane_stack[0]                     # Target pane to split
+    ])
+    pane_backtrace = p.stdout.strip().split(':')
+
+    # Create morion pane
+    p = run_cmd([
+        'tmux', 'split-window',                 # Split pane
+        '-d',                                   # Do not change active pane
+        '-h',                                   # Horizontal split
+        '-P', '-F', '#{pane_id}:#{pane_tty}',   # Print information
+        '-p', '50',                             # Percentage of new pane
+        '-t', pane_pwndbg[0]                    # Target pane to split
+    ])
+    pane_morion = p.stdout.strip().split(':')
+
+    # Configure panes
+    cmd = f'''
 python
 from pwndbg.commands.context import contextoutput
-contextoutput("disasm", "{pane_disasm.pane_tty:s}", True)
+contextoutput("stack", "{pane_stack[1]:s}", True)
+contextoutput("backtrace", "{pane_backtrace[1]:s}", True)
+contextoutput("disasm", "{pane_disasm[1]:s}", True)
+contextoutput("regs", "{pane_regs[1]:s}", True)
 end
-# shell clear
-"""
-    )
+    '''
+    p = run_cmd([
+        'tmux', 'send',
+        '-t', session_name,
+        cmd, 'ENTER'
+    ])
+    
+    p = run_cmd([
+        'tmux', 'send',
+        '-t', f'{session_name:s}.{pane_morion[0]:s}',
+        'morion -h', 'ENTER'
+    ])
+
+    # Run GDB command files
+    for gdb_cmd_file in args.get('gdb_cmd_files', []):
+        p = run_cmd([
+            'tmux', 'send',
+            '-t', session_name,
+            f'source {gdb_cmd_file:s}', 'ENTER'
+        ])
 
     # Attach tmux session
-    server.attach_session(session_name)
+    p = run_cmd([
+        'tmux', 'attach',
+        '-t', session_name
+    ])
+
     return
 
 
