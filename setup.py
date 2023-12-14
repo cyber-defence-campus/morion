65fe140626da25313b4df325c2c18ce6d4c2a5b6
Damian Pfammatter <pfammatter.damian@gmail.com>
2023-12-14 14:18:31 +0100

    Increment bugfix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'morion',
-    version = '0.1.0',
+    version = '0.1.1',
     packages = find_packages(include = ['morion', 'morion.*']),
     install_requires = [
         'termcolor==2.3.0',
